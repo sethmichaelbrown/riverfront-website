4d3ebdf0eb70a1d6043d9ad56aa14350dae66562
SMB <sethmichaelbrown@gmail.com>
2018-10-30 15:22:06 -0600

    first commit for riverfront project

---
+++ standard.js
@@ -0,0 +1,18 @@
+// NavBar
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
