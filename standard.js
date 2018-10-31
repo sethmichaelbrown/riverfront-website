b09fd036e21ae9715c3853b6f37a893d2d97d10b
SMB <sethmichaelbrown@gmail.com>
2018-10-31 10:36:31 -0600

    product layout updated & begin repsonsive design

--- standard.js
+++ standard.js
@@ -1,21 +1,33 @@
-// NavBar
 document.addEventListener('DOMContentLoaded', function() {
+
+// NavBar
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
-});
 
 //Contact Page Dropdown
-document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems);
-});
 
 //Submit Button Action
-document.addEventListener('DOMContentLoaded', function() {
+
   let field = document.querySelector(".form-set-field")
   let submit = document.querySelector("#submit-button")
 
   submit.addEventListener("click", function(event){
     alert("Thank you for you inquiry, we'll reach out soon!")
-  })
-})
+  }) // Submit Button End
+
+//Favorite Button - Local Storage
+
+  let favorite = document.querySelector(".product-wrapper")
+  let aidName = document.querySelector(".card-title")
+
+  favorite.addEventListener("click", function(){
+    localStorage.setItem("favorite", aidName.innerText)
+  }) // Fav Button End
+
+
+
+
+
+}); // DOMContentLoaded End
