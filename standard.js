fe81d3c0a0abf7fa59aaac92d94bfe5d6c7cc515
SMB <sethmichaelbrown@gmail.com>
2018-10-30 20:31:54 -0600

    committing product page + updates to home & contact

--- standard.js
+++ standard.js
@@ -4,7 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems);
 });
 
+//Contact Page Dropdown
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems);
 });
+
+//Submit Button Action
+document.addEventListener('DOMContentLoaded', function() {
+  let field = document.querySelector(".form-set-field")
+  let submit = document.querySelector("#submit-button")
+
+  submit.addEventListener("click", function(event){
+    alert("Thank you for you inquiry, we'll reach out soon!")
+  })
+})
