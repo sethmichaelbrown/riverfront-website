cfb1cb7fe23a5115d15e4c9e5e23080785bb677a
SMB <sethmichaelbrown@gmail.com>
2018-11-06 17:28:56 -0700

    Removing alerts, for they are tacky

--- standard.js
+++ standard.js
@@ -11,15 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
 //Submit Button Action
   let submit = document.querySelector("#submit-button")
   let name = document.querySelector(".first-name")
+  let form = document.querySelector("form")
+  let remove = document.querySelector(".remove")
+  let message = document.createElement("div")
+  let ok = document.createElement("a")
 
-  submit.addEventListener("click", function(){
-    if(name.innerText != ""){
-      alert("Thank you for your inquiry! We will reach out soon.")
-    }
-    else{
-      alert("Please enter valid name or email")
+  submit.addEventListener("click", function(event){
       event.preventDefault()
-    }
+      remove.removeChild(form)
+
+      message.setAttribute("class", "message-form")
+      message.innerHTML = "Thank you for your inquiry! We will reach out soon."
+
+      ok.setAttribute("class", "waves-effect ok-button waves-light btn-large orange darken-1 blue-grey-text hoverable")
+      ok.innerHTML = "OK"
+
+      remove.appendChild(message)
+      remove.appendChild(ok)
+
+      ok.addEventListener("click", function(){
+        document.location.reload()
+      })
   }) // End Submit Button Action
 
 
